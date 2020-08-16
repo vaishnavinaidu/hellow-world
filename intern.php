e01b9642fef82729e195f4b1a766b55b7472e499
vaishnavi nnaidu <68643484+vaishnavinaidu@users.noreply.github.com>
2020-08-16 14:25:29 +0530

    Create intern.php

---
+++ intern.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+  <div class="container">
+  	<div class="row">
+      <div class="col-lg-12">
+      	<h2>Internship Application form</h2>
+
+      	       
+
+
+      </div>
+  	</div>
+  </div>
+</body>
+</html>
